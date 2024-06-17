4df41e4589f978f8e12de7f3e181ed3580ab796f
Nitikesh <nitikeshyewale2016@gmail.com>
2024-06-17 22:49:43 +0530

    final submit1

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 
 const apiUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
-const key="44QHSCQEF2DFVGWHACA6VQSUX";
+const key = "44QHSCQEF2DFVGWHACA6VQSUX";
 
 async function checkWeather(city) {
     //for Current weather
@@ -23,16 +23,16 @@ async function checkWeather(city) {
     let localTime = d.getTime();
     let localOffset = d.getTimezoneOffset() * 60000;
     let utc = localTime + localOffset
-    var cityDate = utc + (data.tzoffset*60*60000);
+    var cityDate = utc + (data.tzoffset * 60 * 60000);
     let showCityDate = new Date(cityDate);
-    var sym="";
-    if(data.tzoffset>0)sym="+";
-    
-    document.querySelector(".date").innerHTML = showCityDate.toDateString() + " " + showCityDate.toTimeString().substring(0, 9)+" <br>GMT"+sym+data.tzoffset+" ("+data.timezone+")";
+    var sym = "";
+    if (data.tzoffset > 0) sym = "+";
+
+    document.querySelector(".date").innerHTML = showCityDate.toDateString() + " " + showCityDate.toTimeString().substring(0, 9) + " <br>GMT" + sym + data.tzoffset + " (" + data.timezone + ")";
 
     //for current user date
-    var sysDate=new Date().toString();
-    document.querySelector(".systemDate").innerHTML = "" + sysDate.substring(0,24)+"<br>"+sysDate.substring(24,);//.toDateString() + " " + new Date().toTimeString().substring(0, 9)+" <br>("+new Date().toTimeString().substring(19,new Date().toTimeString().length-1)+")";
+    var sysDate = new Date().toString();
+    document.querySelector(".systemDate").innerHTML = "" + sysDate.substring(0, 24) + "<br>" + sysDate.substring(24,);//.toDateString() + " " + new Date().toTimeString().substring(0, 9)+" <br>("+new Date().toTimeString().substring(19,new Date().toTimeString().length-1)+")";
 
     document.querySelector(".description").innerHTML = data.days[0].description;
 
@@ -43,21 +43,21 @@ async function checkWeather(city) {
     weatherIcon.src = "https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/2nd%20Set%20-%20Color/" + data.currentConditions.icon + ".svg";//console.log(weatherIcon.src);
 
     //for 7 days weather forecast
-   
+
     var x = 1;
     var previousDate = 0;
     for (i = 0; i < 16; i++) {
-        
-            document.getElementById("date" + x).innerHTML = new Date(data.days[i].datetimeEpoch * 1000).toDateString();
-            document.getElementById("img" + x).src = "https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/2nd%20Set%20-%20Color/" + data.days[i].icon + ".svg";
-            let ls = data.days[i];
-            document.getElementById("temp" + x).innerHTML = Math.round(Math.min(ls.feelslike, ls.feelslikemax, ls.feelslikemin, ls.temp, ls.tempmin, ls.tempmax)) + "°C / " + Math.round(Math.max(ls.feelslike, ls.feelslikemax, ls.feelslikemin, ls.temp, ls.tempmin, ls.tempmax)) + "°C";
-            document.getElementById("desc" + x).innerHTML = data.days[i].conditions;
-            x++;
-            if (x > 7) break;
+
+        document.getElementById("date" + x).innerHTML = new Date(data.days[i].datetimeEpoch * 1000).toDateString();
+        document.getElementById("img" + x).src = "https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/2nd%20Set%20-%20Color/" + data.days[i].icon + ".svg";
+        let ls = data.days[i];
+        document.getElementById("temp" + x).innerHTML = Math.round(Math.min(ls.feelslike, ls.feelslikemax, ls.feelslikemin, ls.temp, ls.tempmin, ls.tempmax)) + "°C / " + Math.round(Math.max(ls.feelslike, ls.feelslikemax, ls.feelslikemin, ls.temp, ls.tempmin, ls.tempmax)) + "°C";
+        document.getElementById("desc" + x).innerHTML = data.days[i].conditions;
+        x++;
+        if (x > 7) break;
 
 
-        
+
     }
     console.log(document.getElementById("app").innerText);
     get(city);
